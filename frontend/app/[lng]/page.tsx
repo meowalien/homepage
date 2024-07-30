f40671fe250afb9e3117b0d3b64694a4b0097637
say ken <a.meowalien@gmail.com>
2024-07-30 18:51:24 +0800

    before use react-markdown

--- frontend/app/[lng]/page.tsx
+++ frontend/app/[lng]/page.tsx
@@ -7,12 +7,13 @@ export default async function Page({params: {lng}}: PageProps) {
     const {t} = await useTranslation(lng, "translation");
 
     return (
-        <>
-            <h1 className="text-amber-500">{t("title")}</h1>
-            <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link>
-            <br/>
-            <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link>
-            <Footer lng={lng}/>
-        </>
+        <></>
+        // <>
+        //     <h1 className="text-red-500">{t("title")}</h1>
+        //     <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link>
+        //     <br/>
+        //     <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link>
+        //     <Footer lng={lng}/>
+        // </>
     );
 }
