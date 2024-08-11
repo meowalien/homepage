b30a0a8e58535f1cbae5bed7dc9f7b4f86129cc3
say ken <a.meowalien@gmail.com>
2024-08-11 18:12:04 +0800

    use main tag

--- components/PageContainer/index.tsx
+++ components/PageContainer/index.tsx
@@ -5,17 +5,13 @@ export type PageProps = { params: { lng: string } };
 
 const PageContainer = async ({children, params: {lng}}: { children?: ReactNode } & PageProps) => {
     return (
-        // <div className="bg-main-page-background w-full flex flex-col items-center">
-        // <GrayBackground>
         <>
-            <div
+            <main
                 className="page:mt-6 mt-0 print:mt-6 mb-8 w-full max-w-[980px] border-0 page:border-[1px] border-middle-gray bg-white print:border-0">
                 {children}
-            </div>
+            </main>
             <ChangeLanguageLink lng={lng} className="print:hidden"/>
         </>
-        // </GrayBackground>
-        // </div>
     )
 }
 
